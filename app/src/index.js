8a0de4e0e67e7de7eaa115b0793e2b2430aa7615
jaferidrees <jaferll@gmail.com>
2023-01-12 15:10:30 +0100

    Initial commit

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import './index.css';
+import App from './App';
+import store from './redux/store';
+
 
 ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+
+  <>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="mainContainer">
+            <App />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </>,
+document.getElementById('root')
 );
--- app/src/redux/store.js
+++ app/src/redux/store.js
@@ -1,2 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import campaignsReducer from './campaign'
 
+const store = configureStore({
+    reducer: campaignsReducer,
+})
+
+export default store;
